f0a85bfedaace43376cb430eb84d38a030722c09
ahd <ahdhani0@gmail.com>
2021-02-16 03:54:46 +0300

    add particles to switch cases

--- src/client/src/World/components/Scene/index.tsx
+++ src/client/src/World/components/Scene/index.tsx
@@ -3,6 +3,7 @@ import { Flex, Box } from '@react-three/flex';
 import { Suspense, useState, useEffect, useCallback } from 'react';
 import BasicPlane from '../BasicPlane';
 import CustomShaderOne from '../CustomShaderOne';
+import Particles from '../Particles';
 const Scene = ({ myVid, otherVid, effect, envIndex }: { myVid: any; otherVid: any; effect: any; envIndex: any }) => {
   const [dir, setDir] = useState<'column' | 'row'>('row');
   const { size, viewport, aspect } = useThree();
@@ -11,15 +12,16 @@ const Scene = ({ myVid, otherVid, effect, envIndex }: { myVid: any; otherVid: an
   const [state, setState] = useState(true);
   const renderer = useCallback(
     (feed: any) => {
-      return <CustomShaderOne vidFeed={feed} />;
-      // switch (parseInt(envIndex)) {
-      //   case 0:
-      //     return <BasicPlane vidFeed={feed} effect={effect} />;
-      //   case 1:
-      //     return <CustomShaderOne vidFeed={feed} />;
-      //   default:
-      //     return null;
-      // }
+      switch (parseInt(envIndex)) {
+        case 0:
+          return <BasicPlane vidFeed={feed} effect={effect} />;
+        case 1:
+          return <CustomShaderOne vidFeed={feed} />;
+        case 2:
+          return <Particles vidFeed={feed} />;
+        default:
+          return null;
+      }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [envIndex]
